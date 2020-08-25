e94723d4b9f89527d46a2d48062b29814f14bee2
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-25 22:29:09 +0500

    working on CountryPicker component - fetched countries data - added the fectched country names in CountryPicker component

--- src/api/index.js
+++ src/api/index.js
@@ -32,3 +32,15 @@ export const fetchDailyData = async () => {
         console.log('fetchDailyData: ', error);
     }
 };
+
+// fetching the countries
+export const fetchCountries = async () => {
+    try {
+        const { data: { countries } } = await axios.get(`${url}/countries`);
+
+        return countries.map(country => country.name);
+         
+    } catch (error) {
+        console.log('fetchCountries: ', error);
+    }
+};
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import { fetchCountries } from '../../api';
 
 import styles from './CountryPicker.module.css';
 
 const CountryPicker = () => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+        };
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+
     return (
-        <div>
-            <h1>CountryPicker</h1>
-        </div>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" justify="center">
+                <option value="global">Global</option>
+                    {fetchedCountries.map((country, i) => (
+                        <option key={i} value={country}>{country}</option>
+                    ))}
+            </NativeSelect>
+        </FormControl>
     );
 };
 
