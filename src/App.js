c3984a05a64e5f6ae1178812a1a88d0f164788f5
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-26 13:00:30 +0500

    App.js updated - getted the country from GlobalState in App.js for conditional sho for Chart - updated styling in App.module.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Cards, LineChart, PieChart, CountryPicker, Header } from './components';
-
 import { Grid } from '@material-ui/core';
+
+import { GlobalContext } from './context/GlobalState';
 
 import styles from './App.module.css';
 
 function App() {
+	const { country } = useContext(GlobalContext);
+
 	return (
 		<div className={styles.container}>
-			<Grid container spacing={3}>
-				<Grid item xs={12} md={12}>
+			<Grid container spacing={3} direction="row" justify="space-around" alignItems="center">
+				<Grid item xs={12} md={12} zeroMinWidth>
 					<Header />
-				</Grid>
-
-				<Grid item xs={12} md={3}>
-					<Cards />
-					<PieChart />
-				</Grid>
-
-				<Grid item xs={12} md={8}>
 					<CountryPicker />
-					<LineChart />
+				</Grid>
+
+				<Grid item xs={12} md={3} zeroMinWidth>				
+					<Cards />
+				</Grid>
+
+				<Grid item xs={12} md={8} zeroMinWidth>
+					{country ? <PieChart /> : <LineChart />}				
 				</Grid>			
 			</Grid>
 		</div>
